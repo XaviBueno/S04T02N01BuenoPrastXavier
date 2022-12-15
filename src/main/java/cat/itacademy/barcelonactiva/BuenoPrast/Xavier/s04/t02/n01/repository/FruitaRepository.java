a357df179cb2b61e3e14c0db1dafc867cecd8864
XaviBueno <xbuenop@outlook.es>
2022-12-15 10:26:53 +0100

    Entregat

---
+++ src/main/java/cat/itacademy/barcelonactiva/BuenoPrast/Xavier/s04/t02/n01/repository/FruitaRepository.java
@@ -0,0 +1,9 @@
+package cat.itacademy.barcelonactiva.BuenoPrast.Xavier.s04.t02.n01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.barcelonactiva.BuenoPrast.Xavier.s04.t02.n01.model.domain.Fruita;
+
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer> {
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/BuenoPrast/Xavier/s04/t02/n01/S04T02N01BuenoPrastXavierApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.BuenoPrast.Xavier.s04.t02.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01BuenoPrastXavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
